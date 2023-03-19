6e1a6cbb3493818e118b5201fb2e371bdf884c84
FatimaAbdulRauf <128053579+FatimaAbdulRauf@users.noreply.github.com>
2023-03-19 15:21:14 +0500

    Add files via upload

---
+++ Task 3 & 4/Task 3 and Task 4 styling.py
@@ -0,0 +1,26 @@
+simple_message = "simple message"
+print(simple_message)
+simple_message = "new message"
+print(simple_message)
+name = "Mahnoor Gulzar"
+print("Hello", name, "would you like to play table tennis right now?")
+print(name.lower(), name.upper(), name.title())
+
+# Fatima A Rauf 19/03/23 working with strings and whitespaces
+print("Jane Austen wrote,\
+\"It is not what we think or feel that makes us who we are. \
+It is what we do or fail to do.\"")
+famous_person= "Jane Austen"
+message = "\'It is not what we think or feel that makes us who we are.\
+ It is what we do or fail to do.\'"
+print(famous_person, "said:", message)
+name_space = "\n   Fatima Rauf \t  "
+print(name_space.strip(), name_space.lstrip(), name_space.rstrip())
+
+# Fatima A Rauf 19/03/23- arithmetic operators in py
+print(4+4)
+print(10-2)
+print(4*2)
+print(16//2)
+fav_num = 67
+print("My favorite number is: ", fav_num)
---
+++ Task 3 & 4/Task 4 creating lists.py
@@ -0,0 +1,15 @@
+names = ["Duaa", "Noor", "Mahnoor", "Mahrukh"]
+print(names[0])
+print(names[1])
+print(names[2])
+print(names[3])
+message= "is my friend."
+print(names[0], message)
+print(names[1], message)
+print(names[2], message)
+print(names[3], message)
+fav_transport = ["Bike", "Porsche", "Taycan", "truck"]
+print("I would like to own a", fav_transport[0].lower())
+print("Driving a", fav_transport[-1], "would be fun.")
+print(fav_transport[1], "is my favourite car.")
+print("I love to drive my", fav_transport[2])
